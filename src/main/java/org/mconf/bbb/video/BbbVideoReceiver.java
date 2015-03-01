29c3800df9bfd1f7db94f6c6d6631c160d70b61c
Diego <diegosiuniube@gmail.com>
2015-03-01 17:29:30 -0300

    alteracoes

--- src/main/java/org/mconf/bbb/video/BbbVideoReceiver.java
+++ src/main/java/org/mconf/bbb/video/BbbVideoReceiver.java
@@ -21,11 +21,7 @@
 
 package org.mconf.bbb.video;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.mconf.bbb.BigBlueButtonClient;
-import org.mconf.bbb.users.Participant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,104 +42,73 @@ public class BbbVideoReceiver {
 		protected void onVideo(Video video) {
 			BbbVideoReceiver.this.onVideo(video);
 		}
-		
-	}
-	
-	private static final Logger log = LoggerFactory.getLogger(BbbVideoReceiver.class);
 
-	private String userId;
+	}
+
+	private static final Logger log = LoggerFactory
+			.getLogger(BbbVideoReceiver.class);
+
 	private String streamName;
 	private VideoConnection videoConnection;
-	
-	public BbbVideoReceiver(String userId, BigBlueButtonClient context, int streamToShow) {
-		this.userId = userId;
+
+	public BbbVideoReceiver(BigBlueButtonClient context, int streamToShow) {
 
 		ClientOptions opt = new ClientOptions();
 		opt.setClientVersionToUse(Utils.fromHex("00000000"));
-		opt.setHost(context.getJoinService().getApplicationService().getServerUrl());
-		opt.setAppName("video/" + context.getJoinService().getJoinedMeeting().getConference());
-		
-		streamName = null;
-		for (Participant p : context.getParticipants()) {
-			if (p.getUserId().equals(userId) && p.hasStream()) {
-				streamName = p.getStatus().getStreamName(streamToShow);
-				break;
-			}
-		}
-		
-		if (streamName == null) {
-			log.debug("The userId = {} has no stream", userId);
-			return;
-		}
-		
+		opt.setHost("192.168.1.3");
+		opt.setAppName("live");
+
+		streamName = "test";
+
 		opt.setWriterToSave(null);
 		opt.setStreamName(streamName);
-		
+
 		videoConnection = new VideoConnection(opt, context);
 	}
-	
+
 	protected void onVideo(Video video) {
 		log.debug("received video package: {}", video.getHeader().getTime());
 	}
-	
+
 	public void start() {
 		if (videoConnection != null)
 			videoConnection.connect();
 	}
-	
+
 	public void stop() {
 		if (videoConnection != null)
 			videoConnection.disconnect();
 	}
-	
-	public String getUserId() {
-		return userId;
-	}
-	
+
 	public String getStreamName() {
 		return streamName;
 	}
-	
+
 	public float getAspectRatio() {
-		return getAspectRatio(userId, streamName);
+		return getAspectRatio(streamName);
 	}
-	
-	public static float getAspectRatio(String userId, String streamName) {
-		if (streamName != null && streamName.contains(userId)) {
-			
-			/*
-			 * 	0.7 -> 120x1601
-			 * 	0.8 -> 120x1601-131292666
-			 * 	0.81 -> 120x160-1-131292666
-			 */
-			
-			/*
-			 * 0.81 stream name format
-			 */
-			Pattern streamNamePattern = Pattern.compile("(\\d+)[x](\\d+)[-]\\d+[-]\\d+");
-			Matcher matcher = streamNamePattern.matcher(streamName);
-			if( matcher.matches() ) {
-				String widthStr = matcher.group(1);
-				String heightStr = matcher.group(2);
-				int width = Integer.parseInt(widthStr);
-				int height = Integer.parseInt(heightStr);
-				return width / (float) height;
-			}
-			
-			/*
-			 * 0.7 or 0.8 stream name format
-			 */
-			streamNamePattern = Pattern.compile("(\\d+)[x](\\d+)([-]\\d+)?");
-			matcher = streamNamePattern.matcher(streamName);
-			if( matcher.matches() ) {				
-				String widthStr = matcher.group(1);
-				String heightAndId = matcher.group(2);
-				String heightStr = heightAndId.substring(0, heightAndId.lastIndexOf(userId));
-				int width = Integer.parseInt(widthStr);
-				int height = Integer.parseInt(heightStr);				
-				return width / (float) height;
-			}			
-		}		
+
+	public static float getAspectRatio(String streamName) {
+		/*
+		 * if (streamName != null) {
+		 * 
+		 * Pattern streamNamePattern = Pattern
+		 * .compile("(\\d+)[x](\\d+)[-]\\d+[-]\\d+"); Matcher matcher =
+		 * streamNamePattern.matcher(streamName);
+		 * 
+		 * if (matcher.matches()) { String widthStr = matcher.group(1); String
+		 * heightStr = matcher.group(2); int width = Integer.parseInt(widthStr);
+		 * int height = Integer.parseInt(heightStr); return width / (float)
+		 * height; }
+		 * 
+		 * streamNamePattern = Pattern.compile("(\\d+)[x](\\d+)([-]\\d+)?");
+		 * matcher = streamNamePattern.matcher(streamName); if
+		 * (matcher.matches()) { String widthStr = matcher.group(1); String
+		 * heightAndId = matcher.group(2); String heightStr =
+		 * heightAndId.substring(0, heightAndId.le); int width =
+		 * Integer.parseInt(widthStr); int height = Integer.parseInt(heightStr);
+		 * return width / (float) height; } }
+		 */
 		return -1;
 	}
 }
