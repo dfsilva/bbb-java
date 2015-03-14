6a2e14f92f0a037950fb0669f323710ff4055575
Diego <diegosiuniube@gmail.com>
2015-03-13 21:45:59 -0300

    alteracoes

--- src/main/java/org/mconf/bbb/video/BbbVideoPublisher.java
+++ src/main/java/org/mconf/bbb/video/BbbVideoPublisher.java
@@ -29,18 +29,17 @@ import com.flazr.rtmp.RtmpReader;
 import com.flazr.rtmp.client.ClientOptions;
 import com.flazr.util.Utils;
 
-
 public class BbbVideoPublisher {
-	private static final Logger log = LoggerFactory.getLogger(BbbVideoPublisher.class);
+	private static final Logger log = LoggerFactory
+			.getLogger(BbbVideoPublisher.class);
 
 	private VideoPublisherConnection videoConnection = null;
-	private String streamName;
 	private BigBlueButtonClient context;
 	private ClientOptions opt;
-	
-	public BbbVideoPublisher(BigBlueButtonClient context, RtmpReader reader, String streamName) {
+
+	public BbbVideoPublisher(BigBlueButtonClient context, RtmpReader reader,
+			String streamName) {
 		this.context = context;
-		this.streamName = streamName;
 
 		opt = new ClientOptions();
 		opt.setClientVersionToUse(Utils.fromHex("00000000"));
@@ -50,29 +49,31 @@ public class BbbVideoPublisher {
 		opt.setStreamName(streamName);
 		opt.setReaderToPublish(reader);
 	}
-	
+
 	public void setLoop(boolean loop) {
-		opt.setLoop(loop? Integer.MAX_VALUE: 0);
+		opt.setLoop(loop ? Integer.MAX_VALUE : 0);
 	}
-	
+
 	public void start() {
 		if (videoConnection == null) {
 			videoConnection = new VideoPublisherConnection(opt, context);
 			videoConnection.connect();
 		}
 	}
-	
+
 	public void stop() {
-		// when the stream is removed from the users module, the client automatically
+		// when the stream is removed from the users module, the client
+		// automatically
 		// receives a NetStream.Unpublish.Success, then the channel is closed
 		// \TODO it's may create a memory leak, check it
-		//videoConnection.disconnect();
+		// videoConnection.disconnect();
 		videoConnection = null;
 	}
 
 	public void fireFirstFrame() {
 		if (videoConnection != null) {
-			videoConnection.publisher.fireNext(videoConnection.publisher.channel, 0);
+			videoConnection.publisher.fireNext(
+					videoConnection.publisher.channel, 0);
 		}
 	}
 }
